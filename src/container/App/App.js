3974c2c021a1e49eae94427e4c540cbe5729c506
Kowshik Ahmed <kowshikahmed92@gmail.com>
2018-07-19 00:47:00 +0600

    file uploaded to this repo

---
+++ src/container/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Aux from "../hoc/Aux";
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello World!!</h1>
+    )
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./container/App/App";
+import style from "./index.css";
+
+ReactDOM.render(<App/>, document.getElementById("app"));
